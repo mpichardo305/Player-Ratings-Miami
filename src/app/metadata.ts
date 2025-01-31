f8a8d2a7acb8501aed92cd95b2317ac3d2a55deb
Michael Pichardo <michael.pichardo@reeftechnology.com>
2025-01-31 00:34:22 -0500

    added open graph preview

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,4 +1,25 @@
 export const metadata = {
-  title: "Player Ratings",
-  description: "Rate and review players easily.",
+  title: "Player Ratings Miami",
+  description: "Rate players in Miami's pickup games!",
+  openGraph: {
+    title: "Player Ratings Miami",
+    description: "Rate players in Miami's pickup games!",
+    url: "https://player-ratings-miami.vercel.app",
+    siteName: "Player Ratings Miami",
+    images: [
+      {
+        url: "/social-preview.jpg", // Path to your image in public/
+        width: 1200,
+        height: 630,
+        alt: "Player Ratings App in Miami",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Player Ratings Miami",
+    description: "Rate players in Miami's pickup games!",
+    images: ["/social-preview.jpg"], // Twitter preview
+  },
 };
