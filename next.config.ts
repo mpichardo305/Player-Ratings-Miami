c89fc640781be8a85d0db100904ce8f969f55c86
Michael Pichardo <michael.pichardo@reeftechnology.com>
2025-02-05 19:13:13 -0500

    Revert "fixing build issues by updating nextConfig"
    This reverts commit 4996893efe8d9542e94ffeb1abef3535e5925f46.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   env: {
     TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
     TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-  },
-  experimental: {
-    serverActions: true,
   }
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
