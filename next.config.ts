bcd4e35fdd4dde8fb8c37041b75a425dd1ef5278
Michael Pichardo <michael.pichardo@reeftechnology.com>
2025-02-05 19:13:01 -0500

    Revert "fixing build issues by updating nextConfig part 2"
    This reverts commit ba8df911d35b00b0f19357a701a7ceb6d3cdfda9.

--- next.config.ts
+++ next.config.ts
@@ -6,18 +6,6 @@ const nextConfig = {
   },
   experimental: {
     serverActions: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-        ],
-      },
-    ]
   }
 }
 
