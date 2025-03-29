a94d9b44ab008f89452cc6eee2d9e0dc54d43aba
Michael Pichardo <michael.pichardo@reeftechnology.com>
2025-03-29 16:35:17 -0400

    adding types and vercel.json

--- src/app/invite/[token]/layout.tsx
+++ src/app/invite/[token]/layout.tsx
@@ -21,6 +21,7 @@ export const metadata: Metadata = {
         width: 1200,
         height: 630,
         alt: "Join Player Ratings Miami",
+        type: "image/jpeg",
       },
     ],
     type: "website",
